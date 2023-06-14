44299b2f3b7fc346e4275a16ed0710fb7d43ae25
stonkmem <73430589+stonkmem@users.noreply.github.com>
2023-06-14 13:22:09 +0800

    Add all terms

--- Article 2/sim.py
+++ Article 2/sim.py
@@ -14,17 +14,20 @@ k = 16.77
 rho_ice = 916.7
 rho_water = 997
 L = 6e3
-P = 50
-while t<10:
-    l = pow(pow(0.05, 3) - t*P/(L * rho_ice), 1/3)
+P = 1
+while (t<3 and l>0):
+    l = max(0, pow(max(0, pow(0.3, 3) - t*P/(L * rho_ice)), 1/3))
     h_t.append(h)
     t_t.append(t)
     hhh = -rho_water * pow(l, 2) * 9.81 * h - k * l * hh + hh * P/L
-    dummy.append(l)
+    dummy.append(pow(l, 3))
     hh += hhh*dt
     h += hh*dt
     t += dt
     hhh = 0
 plt.plot(t_t, h_t)
+plt.plot(t_t, [h_t[-1] for i in t_t])
+# plt.plot(t_t, dummy)
+print(len(t_t))
 # for i in dummy:
 #     print(i)
